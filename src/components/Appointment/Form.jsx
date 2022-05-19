5fb8825c16a85b2ea2c13e3ddf0bdc6a53bd0edd
Kelly Atmore <kelly.atmore47@gmail.com>
2022-05-19 15:17:04 -0700

    add axios and first get request

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -10,15 +10,20 @@ export default function Form(props) {
     setStudent("")
     setInterviewer(null)
   }
+  const cancel = () => {
+    reset()
+    props.onCancel();
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form onSubmit={event => event.preventDefault()} autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
-            name={props.student}
+            name="name"
             type="text"
             placeholder="Enter Student Name"
+            value={student}
             onChange={(event) => setStudent(event.target.value)}
           />
         </form>
@@ -30,7 +35,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onConfirm}>Save</Button>
         </section>
       </section>
