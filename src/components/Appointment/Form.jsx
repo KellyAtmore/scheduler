ae34cdfaab031e9d493dbe21057f11319272490b
Kelly Atmore <kelly.atmore47@gmail.com>
2022-05-18 17:00:32 -0700

    add form component

---
+++ src/components/Appointment/Form.jsx
@@ -0,0 +1,39 @@
+
+import React, { useState } from 'react';
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const reset = () => {
+    setStudent("")
+    setInterviewer(null)
+  }
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name={props.student}
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+        interviewers={props.interviewers} 
+        onChange = {setInterviewer}
+        value = {interviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button confirm onClick={props.onConfirm}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,13 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -143,6 +150,13 @@ storiesOf("Button", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer.name} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+  .add("Confirm", () => <Confirm message={"Delete the appointment?"} onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+  .add("Status", () => <Status message={"Deleting"} />)
+  .add("Error", () =>  <Error message={"Could not delete appointment"} onClose={action("onClose")}  />)
+  .add("Edit", () => <Form student={"Kelly Atmore"} interviewer={2} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}  />)
+  .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />);
 
         
